24626ef1171ebd86c53b5f3d64bc1981f75538ca
Baileigh Remy <baileigh.remy@gmail.com>
2024-10-13 18:05:05 -0500

    Added start of local storage

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,59 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+const submissionInfo = document.getElementById('submission');
+const nameInput = document.getElementById('name');
+const genderInput = document.getElementById('gender');
+const ageInput = document.getElementById('age');
 
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
+const profiles = JSON.parse(localStorage.getItem("profiles")) || [];
 
-    alert("Thanks for submitting!");
+const addProfile = (name, gender, age) => {
+    profiles.push({
+        name, 
+        gender, 
+        age
+    });
+
+    localStorage.setItem("profiles", JSON.stringify(profiles));
+    console.log(localStorage.getItem("profiles"));
+
+    return { name, gender, age };
+};
+
+const createProfileElement = ({name, gender, age}) => {
+    const profileDiv = document.createElement('div');
+    const profileName = document.createElement('h2');
+    const profileGender = document.createElement('p');
+    const profileAge = document.createElement('p');
+
+    profileName.innerText = "Profile name: " + name
+    profileGender.innerText = "Profile gender: " + gender;
+    profileAge.innerText = "Profile age: " + age;
+
+    profileDiv.appendChild(profileName);
+    profileDiv.appendChild(profileGender);
+    profileDiv.appendChild(profileAge);
+
+    const profileContainer = document.getElementById('profileContainer'); // Ensure you have a container in your HTML
+    profileContainer.appendChild(profileDiv);
+
+};
+
+profiles.forEach(createProfileElement);
+
+submissionInfo.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("Button clicked");
+
+const newProfile = addProfile(
+    nameInput.value,
+    genderInput.value,
+    ageInput.value,
+);
+
+createProfileElement(newProfile);
+
+nameInput.value = "";
+genderInput.value = "";
+ageInput.value = "";
+
+
 });
-=======
->>>>>>> 97f16b2e4a12ba187b057624665e2cd0aa94aaea
-
-=======
->>>>>>> 5fcd0cf3c4ef276ed241b617053623f700082c2e
